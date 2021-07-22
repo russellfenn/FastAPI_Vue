6682d64273254c0ffc276669804f807a33e343a1
Russell Fenn <rfenn@vt.edu>
2021-07-22 16:33:11 -0400

    Refactored so the question is now a property of the result instead of separate fields.

--- api.py
+++ api.py
@@ -16,11 +16,9 @@ class MathQuestion(BaseModel):
 # Can you see where this is going?
 class MathResult(BaseModel):
     result: Optional[float]
-    operation: str
-    arg1: float
-    arg2: float
     is_error: bool = False
     msg: Optional[str]
+    question: MathQuestion
 
 
 app = FastAPI()
@@ -35,32 +33,30 @@ def read_index():
 # POST a simple math problem
 @app.post("/math")
 async def math(payload: MathQuestion) -> MathResult:
-    response = {"arg1": payload.arg1,
-                "arg2": payload.arg2,
-                "operation": payload.operation,
-                }
+    response: MathResult = MathResult(question=payload)
     try:
         if payload.operation in ["add", "plus", "sum"]:
-            response["result"] = payload.arg1 + payload.arg2
+            response.result = payload.arg1 + payload.arg2
         elif payload.operation in ["sub", "minus", "subtract", "difference"]:
-            response["result"] = payload.arg1 - payload.arg2
+            response.result = payload.arg1 - payload.arg2
         elif payload.operation in ["multiply", "product", "mult"]:
-            response['result'] = payload.arg1 * payload.arg2
+            response.result = payload.arg1 * payload.arg2
         elif payload.operation in ["exp", "exponent"]:
-            response['result'] = payload.arg1 ** payload.arg2
+            response.result = payload.arg1 ** payload.arg2
         elif payload.operation in ["mod", "modulus"]:
-            response['result'] = payload.arg1 % payload.arg2
+            response.result = payload.arg1 % payload.arg2
         elif payload.operation in ["divide", "div"]:
-            response['result'] = payload.arg1 / payload.arg2
+            response.result = payload.arg1 / payload.arg2
         else:
             response.update({"is_error": True,
                              "msg": f"I don't know how to {payload.operation}!"})
     except (DivisionByZero, InvalidOperation) as e:
-        response.update({"is_error": True,
-                         "msg": f"{e}"})
-    return MathResult(**response)
+        response.is_error = True
+        response.msg = f"{e}"
+    return response
 
 
 @app.get('/add/{arg1}/{arg2}')
 async def add(arg1: float, arg2: float) -> MathResult:
-    return MathResult(result=arg1 + arg2, is_error=False)
+    question: MathQuestion = MathQuestion(operation="add", arg1=arg1, arg2=arg2)
+    return await math(question)
