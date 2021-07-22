9a1b67909651e1e26658ee9fcaeee4282691a0b0
Russell Fenn <rfenn@vt.edu>
2021-07-22 14:19:49 -0400

    Switched to Decimal; more math functions.
    Python floating point math often gives unexpected results like 1.1 + 2.2 => 3.3000000000000003
    Decimal math works the same way as the math we learned in school.

--- api.py
+++ api.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from pydantic import BaseModel, Field
 from typing import Optional
-from decimal import Decimal
+from decimal import Decimal, DivisionByZero, InvalidOperation
 
 # Data Model
 
@@ -22,7 +22,9 @@ class MathResult(BaseModel):
     is_error: bool = False
     msg: Optional[str]
 
+
 app = FastAPI()
+
 
 # Load the Vue web ui from app.html
 @app.get("/app")
@@ -37,16 +39,28 @@ async def math(payload: MathQuestion) -> MathResult:
                 "arg2": payload.arg2,
                 "operation": payload.operation,
                 }
-    if payload.operation == "add":
-        response["result"] = payload.arg1 + payload.arg2
-    elif payload.operation in ["sub", "subtract"]:
-        response["result"] = payload.arg1 - payload.arg2
-    else:
+    try:
+        if payload.operation in ["add", "plus", "sum"]:
+            response["result"] = payload.arg1 + payload.arg2
+        elif payload.operation in ["sub", "minus", "subtract", "difference"]:
+            response["result"] = payload.arg1 - payload.arg2
+        elif payload.operation in ["multiply", "product", "mult"]:
+            response['result'] = payload.arg1 * payload.arg2
+        elif payload.operation in ["exp", "exponent"]:
+            response['result'] = payload.arg1 ** payload.arg2
+        elif payload.operation in ["mod", "modulus"]:
+            response['result'] = payload.arg1 % payload.arg2
+        elif payload.operation in ["divide", "div"]:
+            response['result'] = payload.arg1 / payload.arg2
+        else:
+            response.update({"is_error": True,
+                             "msg": f"I don't know how to {payload.operation}!"})
+    except (DivisionByZero, InvalidOperation) as e:
         response.update({"is_error": True,
-                         "msg":f"I don't know how to {payload.operation}!"})
+                         "msg": f"{e}"})
     return MathResult(**response)
 
 
 @app.get('/add/{arg1}/{arg2}')
 async def add(arg1: float, arg2: float) -> MathResult:
-    return MathResult(result = arg1 + arg2, is_error=False)
+    return MathResult(result=arg1 + arg2, is_error=False)
